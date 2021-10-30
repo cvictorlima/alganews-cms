29b228379a8d6022f83e5036269809b2a68e627c
cvictorlima <89553541+cvictorlima@users.noreply.github.com>
2021-10-30 19:38:04 -0300

    Heading commit input.stories update

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,19 +1,15 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import { Story, Meta } from '@storybook/react';
 import Input, {InputProps} from '../components/Input/Input'
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Input',
   component: Input,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Input>;
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+} as Meta;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+const Template: Story<InputProps> = (args) => <Input {...args} />;
 
 export const WithPlaceholder = Template.bind({})
 WithPlaceholder.args = {
