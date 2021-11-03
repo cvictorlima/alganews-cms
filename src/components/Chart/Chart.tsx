15049bee51bde305d7167aa63205a0d3f5d0e802
cvictorlima <89553541+cvictorlima@users.noreply.github.com>
2021-11-03 20:08:51 -0300

    fixed styled chart chartjs v2

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,89 @@
+import { transparentize } from 'polished';
+import { Line } from 'react-chartjs-2';
+import styled from 'styled-components';
+import Heading from '../Typography/Heading';
+
+const data = {
+  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Set', 'Out', 'Nov', 'Dez'],
+  datasets: [
+    {
+      label: 'Receitas',
+      data: [12, 19, 3, 5, 20, 13, 23, 15, 4, 18, 10, 8],
+      fill: true,
+      backgroundColor: '#0099FF',
+      borderColor: '#0099FF',
+      borderWidth: 0.5,
+      yAxisID: 'vendas',
+    },
+    {
+      label: 'Despesas',
+      data: [1, 20, 13, 18, 5, 14, 15, 4, 18, 6, 9, 20],
+      fill: true,
+      backgroundColor: '#274060',
+      borderColor: '#274060',
+      borderWidth: 0.5,
+      yAxisID: 'vendas',
+    },
+  ],
+};
+
+const options: Chart.ChartOptions = {
+  maintainAspectRatio: true,
+  elements: {
+    line: {
+      tension: 0,
+    },
+  },
+  legend:{
+    display: true,
+    position: 'bottom',
+    align: 'center',
+    labels: {
+      usePointStyle: true,
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        display: true,
+        gridLines: {
+          display:false
+        }
+      }
+    ],
+    yAxes: [
+      {
+        type: 'linear',
+        display: false,
+        position: 'left',
+        id: 'vendas',
+      },
+    ],
+  },
+};
+
+export interface ChartProps {}
+
+export default function Chart () {
+  return <ChartWrapper style={{width:640}}>
+    <div style= {{marginBottom: 16}}>
+      <Heading level={3}>
+        {'Média de performance dos últimos 12 meses'}
+      </Heading>
+    </div>
+    <Line
+      type="line"
+      height={139}
+      width={600}
+      data={data}
+      options={options}
+    />
+  </ChartWrapper>
+}
+
+const ChartWrapper = styled.div`
+  text-align: center;
+  border: 1px solid ${transparentize(0.9, '#274060')};
+  padding: 20px;
+
+`
